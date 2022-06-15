ed6d42eeab8c20a6fb8aa682c7bfa281e73a3f73
VIrreyra <chevi@LAPTOP-8O5H664H>
2022-06-15 15:57:16 -0300

    Josue Virreyra - Inicializacion del TP FINAL

---
+++ src/main/java/ar/edu/unju/edm/Tpfinalgrupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpfinalgrupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpfinalgrupo10Application.class, args);
+	}
+
+}
